c17881ee00349c530f4b269a3abf45939ff7361d
Jeremy-buendia <jcerber343@g.educaand.es>
2024-05-12 18:16:35 +0200

    tonteridas

--- src/unidad1/ejemplo1.java
+++ src/unidad1/ejemplo1.java
@@ -2,22 +2,25 @@ package unidad1;
 
 public class ejemplo1 {
 
-	/*Comentario de lo que esté dentro
-	 * Función main principal de cualquier programa java
-	 * Recibe como parametros los argumentos con los que se haya ejecutado el comando
+	/*
+	 * Comentario de lo que esté dentro Función main principal de cualquier programa
+	 * java Recibe como parametros los argumentos con los que se haya ejecutado el
+	 * comando
 	 */
-	public static void main (String[] args) {
-		//Comentario de una línea
-		
-		//Sacamos por pantalla un mensaje inicial
-		
-		//Definición de variables
-		//variable de Texto que almacena el nombre del lenguaje de programación
+	public static void main(String[] args) {
+		// Comentario de una línea
+
+		System.out.println(System.getProperty("user.home"));
+
+		// Sacamos por pantalla un mensaje inicial
+
+		// Definición de variables
+		// variable de Texto que almacena el nombre del lenguaje de programación
 		String lengProg = new String();
-		
-		//Asignamos el texto Java a la variable
+
+		// Asignamos el texto Java a la variable
 		lengProg = "Java";
-		
+
 		System.out.println("Mi primer programa en " + lengProg);
 	}
 
